b6a43ab69ae2bf087befef9ae0d4a658b0ff485f
lukasz <lukaszkepa03@gmail.com>
2023-01-06 20:56:35 +0100

    footer elements wide fixed

--- src/components/main/footer.js
+++ src/components/main/footer.js
@@ -3,9 +3,9 @@ import { Container, Row, Col, ListGroup } from "react-bootstrap";
 import {BsEnvelopeFill, BsAt} from "react-icons/bs"
 const Footer = () => {
     return (
-        <Container fluid className="pb-3 mt-2 bg-secondary pt-3  fs-3">
-            <Row className="d-flex w-100">
-                <Col className="col-12 col-sm-6 col-md-4">
+        <Container fluid className=" m-0 p-0 pb-3 mt-2 bg-secondary pt-3  fs-3">
+            <Row className="d-flex w-100 justify-content-around m-0">
+                <Col className="col-11 col-sm-5 col-md-4 p-0">
                     <ListGroup className="my-1 fs-4">
                         <ListGroup.Item className="text-break text-white bg-success m-0 p-0 ps-2">Wykorzystane Biblioteki: </ListGroup.Item>
                         <ListGroup.Item className="text-break m-0 p-0 ps-2" >Axios</ListGroup.Item>
@@ -16,7 +16,7 @@ const Footer = () => {
                         <ListGroup.Item className="text-break m-0 p-0 ps-2" >React-router-dom</ListGroup.Item>
                     </ListGroup>
                 </Col>
-                <Col className="col-12 col-sm-6 col-md-4">
+                <Col className="col-11 col-sm-6 col-md-4 p-0">
                     <ListGroup className="my-1 fs-4">
                         <ListGroup.Item className="text-break text-white bg-success m-0 p-0 ps-2">Wykorzystane api: </ListGroup.Item>
                         <ListGroup.Item className="text-break m-0 p-0 ps-2" >(POST) motivational-quotes1.p.rapidapi.com/motivation</ListGroup.Item>
@@ -24,9 +24,9 @@ const Footer = () => {
                         <ListGroup.Item className="text-break m-0 p-0 ps-2" >(GET) pniedzwiedzinski.github.io/<wbr/>kalendarz-swiat-nietypowych</ListGroup.Item>
                     </ListGroup>
                 </Col>
-                <Col className="col-12col-sm-12 col-md-4 text-white">
-                    Projekt opracował Łukasz Kępa&copy;<br/>
-                    Jeżeli masz jakies pytania, skontaktuj się ze mną!<br/>
+                <Col className="col-11  col-md-3 text-white p-0">
+                    <p>Projekt opracował Łukasz Kępa&copy;</p>
+                    <p>Jeżeli masz jakies pytania, skontaktuj się ze mną!<br/></p>
                      <a className="text-warning text-break" href="mailto:lukaszkepa03@gmail.com"><BsEnvelopeFill className="me-1"/>lukaszkepa03<wbr/><BsAt/>gmail.com</a>
                 </Col>
             </Row>
