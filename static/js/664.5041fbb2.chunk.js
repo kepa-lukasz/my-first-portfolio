de297b5b1915b3760ad144fc154b39d8ebaaff0c
lukasz <lukaszkepa03@gmail.com>
2023-01-07 12:23:50 +0100

    Updates

---
+++ static/js/664.5041fbb2.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkr2=self.webpackChunkr2||[]).push([[664],{1664:function(n,r,e){e.r(r);e(2791);var u=e(3360),a=e(184);r.default=function(){return(0,a.jsx)(u.Z,{variant:"danger",children:"404"})}}}]);
+//# sourceMappingURL=664.5041fbb2.chunk.js.map
