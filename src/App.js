f55dbf64150b6595d44f6bab64bb03bb13e41b5e
lukasz <lukaszkepa03@gmail.com>
2023-01-07 12:42:12 +0100

    loader looks better, lazy loading more components

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
-import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
+
 import { HashRouter, Routes, Route } from "react-router-dom";
 import { Row, Container } from 'react-bootstrap';
 import { useState, lazy, Suspense } from 'react';
+import {useCookies} from "react-cookie";
+
 import Navbar from './components/main/navbar';
 import Footer from "./components/main/footer"
 import SwitchElement from "./components/main/switch";
-import Cookies from "./components/main/cookies";
-import Jokes from './components/jokes';
 import Loading from './components/main/loading';
 
-const TicTac = lazy(()=>import( './components/tictac'))
-const Er404 = lazy(()=>import( './components/main/er404'))
-const Day = lazy(()=>import( './components/unusual_days'))
-const Home = lazy(()=>import('./components/home/home'))
+//components loads only when its needed
+const Jokes = lazy(() => import('./components/jokes'))
+const Cookies = lazy(() => import("./components/main/cookies"))
+const TicTac = lazy(() => import('./components/tictac'))
+const Er404 = lazy(() => import('./components/main/er404'))
+const Day = lazy(() => import('./components/unusual_days'))
+const Home = lazy(() => import('./components/home/home'))
 
 
 function App() {
+  const [cookies, setCookie] = useCookies();
+
   //set default value for theme
   const [theme, setTheme] = useState("bg-light text-black p-0")
   let themeHandler = ""
@@ -29,25 +34,25 @@ function App() {
     <HashRouter>
       <Container fluid className={theme} style={{ minHeight: "100vh" }}>
 
-        <Cookies />
-        <Container fluid className="fixed-top text-light bg-success" style={{boxShadow : "0 0 5px #198754"}}>
+
+        <Container fluid className="fixed-top text-light bg-success" style={{ boxShadow: "0 0 5px #198754" }}>
           <SwitchElement handleClick={handleClick} />
         </Container>
 
-        <Container className='p-0 pt-3' fluid style={{marginTop : "3vh"}}>
+        <Container className='p-0 pt-3' fluid style={{ marginTop: "3vh" }}>
           <Navbar />
-
-          <Jokes />
-          <Row className='m-0'>
-            <Suspense fallback={<Loading/>}>
-            <Routes> 
-              <Route path='/' element={<Home />} />
-              <Route path='/tictac' element={<TicTac />} />
-              <Route path='/unusual_day' element={<Day />} />
-              <Route path='/*' element={<Er404 />} />
-            </Routes>
-            </Suspense>
-          </Row>
+          <Suspense fallback={<Loading />}>
+            {(cookies.showJoke !== "0")? <Jokes /> : null}
+            {(cookies.allowCookies !== "1")? <Cookies /> : null}
+            <Row className='m-0'>
+              <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path='/tictac' element={<TicTac />} />
+                <Route path='/unusual_day' element={<Day />} />
+                <Route path='/*' element={<Er404 />} />
+              </Routes>
+            </Row>
+          </Suspense>
 
           <Footer style={{ alignSelf: "flex-end" }} />
         </Container>
--- src/components/main/loading.js
+++ src/components/main/loading.js
@@ -1,9 +1,11 @@
 import React from "react";
-import { Spinner } from "react-bootstrap";
+import { Container, Spinner } from "react-bootstrap";
 
 const Loading = ()=>{
     return(
-        <Spinner animation="border" />
+        <Container className="d-flex justify-content-center py-4">
+            <Spinner className="m-0 p-0 text-success" animation="border" />
+        </Container>
     )
 }
 
